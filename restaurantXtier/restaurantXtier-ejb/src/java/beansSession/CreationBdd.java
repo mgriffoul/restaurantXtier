8691899c84ccfb611723fff4ee55055611eb29eb
mgriffoul <griffoulmathieu@gmail.com>
2016-05-18 14:03:27 +0200

    implementation methode creationBdd

--- restaurantXtier/restaurantXtier-ejb/src/java/beansSession/CreationBdd.java
+++ restaurantXtier/restaurantXtier-ejb/src/java/beansSession/CreationBdd.java
@@ -2,10 +2,26 @@
 package beansSession;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 
 @Stateless
 public class CreationBdd implements CreationBddLocal {
+
+    @PersistenceContext(unitName = "restaurantXtier-ejbPU")
+    private EntityManager em;
+    
+    
+    @Override
+    public void genererBdd() {
+        
+        //jeux de test ici ! 
+        
+        
+        
+        
+    }
 
     
 }
--- restaurantXtier/restaurantXtier-ejb/src/java/beansSession/CreationBddLocal.java
+++ restaurantXtier/restaurantXtier-ejb/src/java/beansSession/CreationBddLocal.java
@@ -1,17 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package beansSession;
 
 import javax.ejb.Local;
 
-/**
- *
- * @author cdi207
- */
+
 @Local
 public interface CreationBddLocal {
+    
+    public void genererBdd();
     
 }
