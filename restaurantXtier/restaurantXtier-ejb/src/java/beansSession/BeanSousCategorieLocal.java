e89d434ce583b5197d983f320fb2ddc65ec9b59d
mgriffoul <griffoulmathieu@gmail.com>
2016-05-23 15:17:02 +0200

    modif git ignore ajout persistence.xml

--- restaurantXtier/restaurantXtier-ejb/src/java/beansSession/BeanSousCategorieLocal.java
+++ restaurantXtier/restaurantXtier-ejb/src/java/beansSession/BeanSousCategorieLocal.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package beansSession;
 
 import beanEntite.SousCategorie;
 import java.util.List;
 import javax.ejb.Local;
 
-/**
- *
- * @author cdi207
- */
+
 @Local
 public interface BeanSousCategorieLocal {
     
