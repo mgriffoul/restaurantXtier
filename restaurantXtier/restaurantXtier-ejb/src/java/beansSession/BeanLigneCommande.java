9d07c9434ee534d4e8ffeaf3fe93357697c24836
BruceGantner <gantnerb@hotmail.com>
2016-05-25 16:56:46 +0200

    ajout methode et modif d'une autre

--- restaurantXtier/restaurantXtier-ejb/src/java/beansSession/BeanLigneCommande.java
+++ restaurantXtier/restaurantXtier-ejb/src/java/beansSession/BeanLigneCommande.java
@@ -36,10 +36,24 @@ public class BeanLigneCommande implements BeanLigneCommandeLocal {
 
     @Override
     public List<LigneCommande> selectAllLigneCommandeTriByPlat() {
-        String req ="select lc from LigneCommande lc order by lc.article.nom";
+        String req ="select lc from LigneCommande lc "
+                + "where lc.article.sousCategorie.categorie.id=4 "
+                + "or lc.article.sousCategorie.categorie.id=5 "
+                + "or lc.article.sousCategorie.categorie.id=2 "
+                + "order by lc.article.nom ";
         Query qr = em.createQuery(req);
         List<LigneCommande> ligneCommandes = qr.getResultList();
         return ligneCommandes;
+    }
+
+    @Override
+    public LigneCommande changerEtatLigneCommande(Long id, String etat) {
+       String req="update lc LigneCommande lc set lc.etat=:paramEtat where lc.id=:paramId";
+       Query qr = em.createQuery(req);
+       qr.setParameter("paramEtat", etat);
+       qr.setParameter("paramId", id);
+       LigneCommande lCom= (LigneCommande) qr.getSingleResult();
+       return lCom;
     }
 
 }
